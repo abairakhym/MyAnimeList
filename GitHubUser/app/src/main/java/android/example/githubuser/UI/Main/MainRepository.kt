0a32c548d3e73bbb654e182a276fa372f68e2d0e
Abai Rakhym <63360983+abairakhym@users.noreply.github.com>
2022-04-07 10:39:26 +0600

    Delete GitHubUser directory

--- GitHubUser/app/src/main/java/android/example/githubuser/UI/Main/MainRepository.kt
+++
@@ -1,8 +0,0 @@
-package android.example.githubuser.UI.Main
-
-import android.example.githubuser.Network.APIUserRequest
-
-class MainRepository constructor(private val retrofit: APIUserRequest) {
-    fun getAllUser() = retrofit.getUserList(10,"latestUserId")
-    fun getUserDetail(id : Int) = retrofit.getUserDetail(id)
-}
